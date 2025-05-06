5fb90506c9899c8d6e29453056ac9e390cf2e046
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-06 11:20:57 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: '172.31.22.204',
+    host: '172.31.92.52 ' #cambiar_por_ip_privada,
     port: 5432,
     user: 'root',
     password: 'usuarios1234',
