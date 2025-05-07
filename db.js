5fd48acad8d23e5d02f37a9ebc89b89b2359c556
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-07 10:23:28 -0500

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-    host: 'IP_DE_TU_NUEVA_MV_DB', // Aquí va la IP de la nueva MV
-    port: 5432,
-    user: 'root',           // Debe coincidir con docker-compose
-    password: 'usuarios1234', // Debe coincidir con docker-compose
-    database: 'ms-usuarios'  // Debe coincidir con docker-compose
-});
-module.exports = pool;
