f93a76d105c465585fa87f3afa26d243e57b7194
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-09 15:08:13 -0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/users');
+const { swaggerUi, specs } = require('../swagger');
 require('dotenv').config();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
 app.use('/usuarios', userRoutes);
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
